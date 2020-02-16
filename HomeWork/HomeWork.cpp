8c935efff4004f2aa0bd4d8703d37bab94580d52
iesm00 <60887580+iesm00@users.noreply.github.com>
2020-02-16 19:40:46 +0300

    Update HomeWork.cpp

--- HomeWork/HomeWork.cpp
+++ HomeWork/HomeWork.cpp
@@ -4,11 +4,13 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
+    cout << "Hello World!\n";
     cout << "Hello World!\n";
     cout << "Hello everyone!\n";
     cout << "Hello everyone!\n";
